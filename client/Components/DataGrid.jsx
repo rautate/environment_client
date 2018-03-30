ae81d0ae964183592e9ef925555c06bf1e648410
dumitrugherciu <dumitrugherciu@gmail.com>
2018-03-30 19:28:26 +0300

    some modifications

--- client/Components/DataGrid.jsx
+++ client/Components/DataGrid.jsx
@@ -10,17 +10,17 @@ const DataGrid = createReactClass({
             <div className='row'>
                 <div className='col-sm-3 panel-actual'>
                     <div>
-                        <span className='t-letter'>T</span>
-                        <span className='float-right'>&#x2103;</span>
+                        <span className='t-letter'>T<span className='text-xxs'>emperature</span></span>
+                        <span className='float-right h4'>&#x2103;</span>
                         <p className='temp-value'>{this.props.data.Temperature}</p>
                     </div>
-                    <div className='humidity'>H <span className='float-right'>40%</span></div>
-                    <div className='pressure'>P <span className='float-right'>{this.props.data.Formaldehyde}kPa</span></div>
+                    <div className='humidity'>H<span className='text-xxs'>umidity</span> <span className='float-right'>40%</span></div>
+                    <div className='pressure'>P<span className='text-xxs'>ressure</span> <span className='float-right'>{this.props.data.Formaldehyde}kPa</span></div>
                 </div>
                 <div className='col-sm-3 panel-actual'>
-                    <div>CO2: {this.props.data.CO2}</div>
-                    <div>CH2OH: {this.props.data.CO2}</div>
-                    <div>VOC: {this.props.data.CO2}</div>
+                    <div className='text-xl'>CO2 {this.props.data.CO2}</div>
+                    <div className='text-xl'>CH2OH {this.props.data.CO2}</div>
+                    <div className='text-xl'>VOC {this.props.data.CO2}</div>
                 </div>
                 <div className='col-sm-3 panel-actual'>
                     <div>PM 0-1: {this.props.data.PM0_1}</div>
