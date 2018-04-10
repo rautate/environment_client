922d2acc872d6c9b2389a284db0932dff2cad2d5
dumitrugherciu <dumitrugherciu@gmail.com>
2018-04-10 18:38:10 +0300

    add automatic update and RAG status for temp

--- client/Components/Actual.jsx
+++ client/Components/Actual.jsx
@@ -2,27 +2,44 @@ import React from 'react';
 import {apiPrefix} from "../../server/etc/config";
 import axios from "axios/index";
 import DataGrid from './DataGrid.jsx';
+import classNames from "classnames";
 
 
 var createReactClass = require('create-react-class');
 
 const Actual = createReactClass({
     getInitialState: function () {
-        return {data: ''}
+        return{
+            data: ''
+        }
     },
-
     componentWillMount() {
+        var intervalID = setInterval(this.updateData, 60000);
+        this.updateData();
+        this.setState({intervalID: intervalID});
+    },
+    componentWillUnmount() {
+        clearInterval(this.state.intervalID);
+    },
+    computeClass: function(val) {
+        return classNames({
+            'temp-value': true,
+            'neutral': val <= 5,
+            'danger': val >= 35,
+            'yellow': val > 5 && val < 35
+        });
+    },
+    updateData: function(){
         axios.get(`${apiPrefix}/actual`)
             .then( response => {
-                this.setState({data: response.data[0]})
+                this.setState({data: response.data[0]});
+                var tempTextClass = this.computeClass(this.state.data.Temperature);
+                this.setState({tempTextClass: tempTextClass});
             })
     },
-
     render(){
         return(
-            <div>
-                <DataGrid data={this.state.data} />
-            </div>
+            <DataGrid data={this.state.data} tempClass={this.state.tempTextClass} />
         );
     }
 });
--- client/Components/DataGrid.jsx
+++ client/Components/DataGrid.jsx
@@ -4,7 +4,6 @@ var createReactClass = require('create-react-class');
 
 
 const DataGrid = createReactClass({
-
     render() {
         return(
             <div className='row'>
@@ -12,23 +11,34 @@ const DataGrid = createReactClass({
                     <div>
                         <span className='t-letter'>T<span className='text-xxs'>emperature</span></span>
                         <span className='float-right h4'>&#x2103;</span>
-                        <p className='temp-value'>{this.props.data.Temperature}</p>
+                        <p className={this.props.tempClass}>{this.props.data.Temperature}</p>
                     </div>
-                    <div className='humidity'>H<span className='text-xxs'>umidity</span> <span className='float-right'>40%</span></div>
-                    <div className='pressure'>P<span className='text-xxs'>ressure</span> <span className='float-right'>{this.props.data.Formaldehyde}kPa</span></div>
+                    <div className='humidity'>H<span className='text-xxs'>umidity</span> <span className='float-right'>{this.props.data.Humidity}%</span></div>
+                    <div className='pressure'>P<span className='text-xxs'>ressure</span> <span className='float-right'>{this.props.data.Pressure} kPa </span></div>
                 </div>
                 <div className='col-sm-3 panel-actual'>
-                    <div className='text-xl'>CO2 {this.props.data.CO2}</div>
-                    <div className='text-xl'>CH2OH {this.props.data.CO2}</div>
-                    <div className='text-xl'>VOC {this.props.data.CO2}</div>
+                    <div className='text-xl'>
+                        <p className='p-gas'>CO2</p>
+                        <p className='gas-values'>{this.props.data.CO2/10000}%</p>
+                        <p className='gas-values'>{this.props.data.CO2*1.8} <span className='text-xxs'>mg/m3</span></p>
+                    </div>
+                    <div className='text-xl'>
+                        <p className='p-gas'>CH2OH</p>
+                        <p className='gas-values'>{this.props.data.Formaldehyde/10000000}% </p>
+                        <p className='gas-values'>{(this.props.data.Formaldehyde/1000*1.25)} <span className='text-xxs'>mg/m3</span></p>
+                    </div>
+                    <div className='text-xl'>
+                        <p className='p-gas'>VOC</p>
+                        <p className='gas-values'>{this.props.data.Gas} k&#8486;</p>
+                    </div>
                 </div>
                 <div className='col-sm-3 panel-actual'>
-                    <div>PM 0-1: {this.props.data.PM0_1}</div>
-                    <div>PM 2-5: {this.props.data.PM2_5}</div>
-                    <div>PM 10: {this.props.data.PM10}</div>
+                    <div className='text-xl'>PM 0-1: {this.props.data.PM0_1} ppm</div>
+                    <div className='text-xl'>PM 2-5: {this.props.data.PM2_5} ppm</div>
+                    <div className='text-xl'>PM 10: {this.props.data.PM10} ppm</div>
                 </div>
             </div>
-        )
+        );
     }
 });
 
